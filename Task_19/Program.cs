5c7ddc1741197ade83d20090425cd1552d52f33d
Ludmila <luda-bolshova@mail.ru>
2023-05-03 23:51:54 +0300

    Добавила задачи по 3 семинару

---
+++ Task_19/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Через строку решать нельзя.
+
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+Console.Write("Введите число: ");
+int NumberOfPalindrom = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Является ли число {NumberOfPalindrom} - палиндромом?");
+if (NumberOfPalindrom == Palindrom(NumberOfPalindrom))
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine($"{NumberOfPalindrom} - > да");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine($"{NumberOfPalindrom} - > нет");
+}
+Console.ResetColor();
+
+int Palindrom(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result = result * 10 + number % 10;
+        number /= 10;
+    }
+    return result;
+}
