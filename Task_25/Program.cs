3dbd4e5e79b7423ad8214bd3359033b7a96c4dc6
Ludmila <luda-bolshova@mail.ru>
2023-05-24 16:08:43 +0300

    Добавила задачу по 4 семинару

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -6,21 +6,20 @@
 2, 4 -> 16
 */
 
-decimal Degree (decimal A, int B)
-{
-    decimal res = 1;
-    for(int i=1; i<= B; i++)
-    {
-        res= res*A;
-    }
-    return res;
-}
+// decimal Degree (decimal A, int B)
+// {
+//     decimal res = 1;
+//     for(int i=1; i<= B; i++)
+//     {
+//         res= res*A;
+//     }
+//     return res;
+// }
 
+//  Console.WriteLine("Введите число A");
+//  decimal A = Convert.ToDecimal(Console.ReadLine());
+//  Console.WriteLine("Введите число B");
+//  int B = Convert.ToInt32(Console.ReadLine());
 
- Console.WriteLine("Введите число A");
- decimal A = Convert.ToDecimal(Console.ReadLine());
- Console.WriteLine("Введите число B");
- int B = Convert.ToInt32(Console.ReadLine());
-
-decimal result = Degree(A, B);
- System.Console.WriteLine($"{A} в степени {B}  = {result}");
+// decimal result = Degree(A, B);
+//  System.Console.WriteLine($"{A} в степени {B}  = {result}");
