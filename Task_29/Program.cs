53ed8cbd9229b23329131b5d05dc66bc4469c91c
Ludmila <luda-bolshova@mail.ru>
2023-05-24 23:09:42 +0300

    добавлена задача к 4 семинару

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -6,4 +6,20 @@
 */
 
 
-// 
+// int[] arrayN (int size)
+// {
+//     int[] res = new int[size];
+   
+//     for(int i=0; i<res.Length; i++)
+//     {
+//         System.Console.WriteLine($"Введите элемент массива с индексом {i}");
+//         res[i] = Convert.ToInt32(Console.ReadLine());
+//         }
+//     return res;
+// }
+
+
+// System.Console.WriteLine("Введите размер массива");
+// int x = Convert.ToInt32(Console.ReadLine());
+// int [] array = new int [x];
+// System.Console.WriteLine(string.Join("  ", arrayN(x)));
