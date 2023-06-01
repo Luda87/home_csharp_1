5d9ed5268f684f2171f389c23a444ec89de50026
Ludmila <luda-bolshova@mail.ru>
2023-06-01 14:45:06 +0300

    Добавила задачи по 6 семинару

---
+++ Task_41/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+
+ int HowManyPosNumbersInArray(int[]array) 
+{ 
+     int count = 0; 
+     for (int i = 0; i < array.Length; i++) 
+     
+         if (array[i] > 0) count++; 
+     return count; 
+ } 
+ 
+ System.Console.WriteLine("Enter elements of array with gaps:"); 
+ int[] NewArray = Array.ConvertAll(Console.ReadLine().Split(), int.Parse); 
+ 
+
+ System.Console.WriteLine($"There are {HowManyPosNumbersInArray(NewArray)} positive numbers in this array."); 
